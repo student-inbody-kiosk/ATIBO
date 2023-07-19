4c8933d03a93e0a768c91b89f72d263d08590556
Yeji Jung <yzii9841@gmail.com>
2023-07-19 17:27:19 +0900

    FE:rename: CSS folder structure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: '@import "@/styles/main.scss";',
+                additionalData: '@import "@/styles/utils/index";',
             },
         },
     },
