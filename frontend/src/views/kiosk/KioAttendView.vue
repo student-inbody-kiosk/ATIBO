30083f29f34c64a5706f90b61e25fc954454b665
Yeji Jung <yzii9841@gmail.com>
2023-07-21 20:56:47 +0900

    FE:feat: create TheInput

--- frontend/src/views/kiosk/KioAttendView.vue
+++ frontend/src/views/kiosk/KioAttendView.vue
@@ -2,18 +2,27 @@
 import KioLayout from '@/components/kiosk/KioLayout.vue';
 import KioHeader from '@/components/kiosk/KioHeader.vue';
 import InputGuide from '@/components/kiosk/InputGuide.vue';
+import TheInput from '@/components/common/TheInput.vue';
 import TheKeypad from '@/components/kiosk/TheKeypad.vue';
 
 import { ref } from 'vue';
 let studentInfo = ref('');
 
-const handleInput = function updateAttendData(value: string) {
+const handleInput = function addStudentInfo(value: string) {
     studentInfo.value += value;
 };
 
-const deleteInput = function deleteAttendData() {
+const deleteInput = function deleteStudentInfo() {
     if (studentInfo.value.length == 0) return;
     studentInfo.value = studentInfo.value.slice(0, -1);
+};
+
+const handleChange = function updateStudentInfo(info: string) {
+    studentInfo.value = info;
+};
+
+const handleSubmit = function checkAttendance() {
+    console.log(studentInfo.value);
 };
 </script>
 
@@ -24,7 +33,13 @@ const deleteInput = function deleteAttendData() {
         </template>
         <template #kiosk-main>
             <InputGuide />
-            <div>{{ studentInfo }}</div>
+            <TheInput
+                type="text"
+                refer="학년반번호"
+                :value="studentInfo"
+                @update-input="handleChange"
+                @submit="handleSubmit" />
+            <button @click="handleSubmit">완료</button>
             <TheKeypad @input="handleInput" @delete="deleteInput" />
         </template>
     </KioLayout>
