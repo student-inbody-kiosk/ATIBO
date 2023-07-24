ca6666a128ebc41a7b10c9d1191ea079824b6f18
Yeji Jung <yzii9841@gmail.com>
2023-07-25 01:10:21 +0900

    FE:fix: rendering different components in KioHeader based on the router name

--- frontend/src/views/kiosk/KioIndexView.vue
+++ frontend/src/views/kiosk/KioIndexView.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-import IconButton from '@/components/common/IconButton.vue';
 import TheButton from '@/components/common/TheButton.vue';
 </script>
 
 <template>
     <div class="kiosk-main_buttons">
-        <IconButton
-            text="관리자"
-            emitMessage="admin"
-            @admin="$router.push({ name: 'admin' })">
-            <template #icon>
-                <font-awesome-icon icon="user-lock" />
-            </template>
-        </IconButton>
         <TheButton
             text="출석 확인"
             color="kiosk-primary"
--- frontend/src/views/kiosk/KioView.vue
+++ frontend/src/views/kiosk/KioView.vue
@@ -2,9 +2,13 @@
 import { RouterView } from 'vue-router';
 import KioLayout from '@/components/kiosk/KioLayout.vue';
 import KioHeader from '@/components/kiosk/KioHeader.vue';
+import IconButton from '@/components/common/IconButton.vue';
 
-import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { ref, watchEffect } from 'vue';
 
+const route = useRoute();
+const isIndexPage = ref(false);
 const title = ref('');
 const routeName = ref('');
 
@@ -15,12 +19,32 @@ const handleBeforeMount = function allocateTitleAndRouteName(data: {
     title.value = data.title;
     routeName.value = data.routeName;
 };
+
+watchEffect(() => {
+    if (route.name === 'kiosk') {
+        isIndexPage.value = true;
+    } else {
+        isIndexPage.value = false;
+    }
+});
 </script>
 
 <template>
     <KioLayout>
         <template #kiosk-header>
-            <KioHeader :title="title" :routeName="routeName" />
+            <IconButton
+                v-show="isIndexPage"
+                text="관리자"
+                emitMessage="admin"
+                @admin="$router.push({ name: 'admin' })">
+                <template #icon>
+                    <font-awesome-icon icon="user-lock" />
+                </template>
+            </IconButton>
+            <KioHeader
+                v-show="!isIndexPage"
+                :title="title"
+                :routeName="routeName" />
         </template>
         <template #kiosk-main>
             <router-view @before-mount="handleBeforeMount" />
