c75735c4e95ffe2bd27c70d37ce4eb1413535593
Yeji Jung <yzii9841@gmail.com>
2023-08-04 11:15:42 +0900

    FE:style: create StudentSearchbar

---
+++ frontend/src/components/admin/StudentSearchbar.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import VInput from '@/components/common/VInput.vue';
+import VButton from '@/components/common/VButton.vue';
+withDefaults(
+    defineProps<{
+        grade: string;
+        room: string;
+        number: string;
+        name: string;
+        query: {};
+        isStudentData: boolean;
+    }>(),
+    {
+        isStudentData: false,
+    }
+);
+</script>
+
+<template>
+    <div class="student-search-bar">
+        <div class="student-search-bar__buttons">
+            <VButton
+                text="학생 추가"
+                color="green"
+                @click="$router.push({ name: 'admin-student-create' })" />
+            <VButton
+                v-if="isStudentData"
+                text="학생 수정"
+                color="admin-primary"
+                @click="
+                    $router.push({ name: 'admin-student-update', query })
+                " />
+            <VButton
+                v-if="isStudentData"
+                text="학생 삭제"
+                color="red"
+                @click="
+                    $router.push({ name: 'admin-student-delete', query })
+                " />
+        </div>
+        <div class="student-search-bar__input">
+            <VInput
+                id="grade"
+                label="학년"
+                refer="grade"
+                :value="grade"
+                @input="(value: string) => $emit('grade', value)" />
+            <VInput
+                id="room"
+                label="반"
+                refer="room"
+                :value="room"
+                @input="(value: string) => $emit('room', value)" />
+            <VInput
+                id="number"
+                label="번호"
+                refer="number"
+                :value="number"
+                @input="(value: string) => $emit('number', value)" />
+            <VInput
+                id="name"
+                label="이름"
+                refer="name"
+                :value="name"
+                @input="(value: string) => $emit('name', value)" />
+            <VButton
+                text="조회"
+                color="admin-primary"
+                @click="$emit('search')" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.student-search-bar {
+    display: flex;
+    flex-direction: column;
+}
+.student-search-bar__buttons {
+    align-self: flex-end;
+}
+.student-search-bar__input {
+    display: flex;
+    align-items: center;
+}
+</style>
--- frontend/src/views/admin/AdmStudentView.vue
+++ frontend/src/views/admin/AdmStudentView.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import VInput from '@/components/common/VInput.vue';
-import VButton from '@/components/common/VButton.vue';
+import StudentSearchbar from '@/components/admin/StudentSearchbar.vue';
 import StudentDetailDataLabel from '@/components/admin/StudentDetailDataLabel.vue';
 import StudentDetailData from '@/components/admin/StudentDetailData.vue';
-
 import { ref, computed } from 'vue';
-
 import { getStudents } from '@/apis/services/students';
-
 import type { Ref } from 'vue';
 import type { StudentDetail } from '@/types/students.interface';
 
@@ -41,58 +37,19 @@ const handleSubmit = function searchStudents() {
 <template>
     <div class="admin-student">
         <div class="admin-student__header">학생 관리</div>
-        <section class="admin-student__searchbar">
-            <VInput
-                id="grade"
-                label="학년"
-                refer="grade"
-                :value="grade"
-                @input="(value) => (grade = value)" />
-            <VInput
-                id="room"
-                label="반"
-                refer="room"
-                :value="room"
-                @input="(value) => (room = value)" />
-            <VInput
-                id="number"
-                label="번호"
-                refer="number"
-                :value="number"
-                @input="(value) => (number = value)" />
-            <VInput
-                id="name"
-                label="이름"
-                refer="name"
-                :value="name"
-                @input="(value) => (name = value)" />
-            <VButton
-                text="조회"
-                color="admin-primary"
-                size="md"
-                @click="handleSubmit" />
-            <VButton
-                text="학생 추가"
-                color="green"
-                size="md"
-                @click="$router.push({ name: 'admin-student-create' })" />
-            <VButton
-                v-if="students.length"
-                text="학생 수정"
-                color="admin-primary"
-                size="md"
-                @click="
-                    $router.push({ name: 'admin-student-update', query })
-                " />
-            <VButton
-                v-if="students.length"
-                text="학생 삭제"
-                color="red"
-                size="md"
-                @click="
-                    $router.push({ name: 'admin-student-delete', query })
-                " />
-        </section>
+        <StudentSearchbar
+            :grade="grade"
+            :room="room"
+            :number="number"
+            :name="name"
+            :query="query"
+            :isStudentData="Boolean(students.length)"
+            @grade="(value) => (grade = value)"
+            @room="(value) => (room = value)"
+            @number="(value) => (number = value)"
+            @name="(value) => (name = value)"
+            @search="handleSubmit" />
+
         <section class="admin-student-list">
             <table class="admin-student-list__table">
                 <StudentDetailDataLabel
@@ -116,8 +73,10 @@ const handleSubmit = function searchStudents() {
 </template>
 
 <style lang="scss" scoped>
-.admin-student__searchbar {
-    display: flex;
+.admin-student {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto minmax(0, 1fr);
 }
 
 .admin-student-list {
