e017fe2291786ea87242b25bbdc6db56f25e83df
Yeji Jung <yzii9841@gmail.com>
2023-08-07 11:19:51 +0900

    FE:feat: inbody detail chart

--- frontend/src/views/admin/AdmInbodyDetailView.vue
+++ frontend/src/views/admin/AdmInbodyDetailView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import VButton from '@/components/common/VButton.vue';
+import InbodyDetailData from '@/components/admin/inbody/InbodyDetailData.vue';
 import InbodyUpdateModal from '@/components/admin/inbody/InbodyUpdateModal.vue';
 import { onBeforeMount, ref } from 'vue';
 import { useRoute } from 'vue-router';
@@ -11,12 +12,27 @@ import { useStudentStore } from '@/stores/student.store';
 const studentStore = useStudentStore();
 const route = useRoute();
 const { grade, room, number, name, inbodyId } = route.params;
-const inbody = ref<InbodyDetail>();
+const inbody = ref<InbodyDetail>({
+    id: undefined,
+    testDate: 'YYYY-MM-DD',
+    weight: 0,
+    percentBodyFat: 0,
+    skeletalMuscleMass: 0,
+    height: 0,
+    age: 0,
+    totalBodyWater: 0,
+    protein: 0,
+    minerals: 0,
+    bodyFatMass: 0,
+    bodyMassIndex: 0,
+    score: 0,
+});
 const isUpdateModalOpen = ref(false);
 
 // 인바디 데이터 조회 API
 const getInbodyAPI = () => {
     getInbody(Number(inbodyId)).then((res) => (inbody.value = res));
+    console.log(inbody.value, 'HERE');
 };
 
 onBeforeMount(() => {
@@ -48,16 +64,16 @@ const handleDeleteClick = function deleteInbodyData() {
 </script>
 
 <template>
-    <div>
+    <div class="admin-inbody-detail">
         <!-- TODO: Inbody Graph -->
-        <h1 class="student-info">
+        <!-- <h1 class="student-info">
             {{
                 `${grade} 학년 ${room} 반 ${number} 번 ${name} (${studentStore?.student?.sex})`
             }}
-        </h1>
+        </h1> -->
         <VButton text="수정" color="admin-primary" @click="handleUpdateClick" />
         <VButton text="삭제" color="red" @click="handleDeleteClick" />
-        <div>{{ inbody }}</div>
+        <InbodyDetailData :name="name" :inbody="inbody" :sex="0" />
         <InbodyUpdateModal
             v-if="isUpdateModalOpen"
             @close-modal="isUpdateModalOpen = false"
@@ -68,6 +84,13 @@ const handleDeleteClick = function deleteInbodyData() {
 </template>
 
 <style lang="scss" scoped>
+.admin-inbody-detail {
+    .admin-inbody-student {
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: auto minmax(0, 1fr);
+    }
+}
 .student-info {
     font-size: 1.5rem;
     text-align: center;
