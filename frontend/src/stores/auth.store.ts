55c847d21b7cf76280fc62a58fb339aa49ab5600
Byongho96 <unlike96@gmail.com>
2023-08-14 20:52:21 +0900

    FE:fix: update auth store

--- frontend/src/stores/auth.store.ts
+++ frontend/src/stores/auth.store.ts
@@ -1,31 +1,28 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
-export const useAuthStore = defineStore(
-    'auth',
-    () => {
+export const useAuthStore = defineStore('auth', {
+    state: () => {
         const refreshToken = ref('');
         const accessToken = ref('');
-        const updateAccessToken = (token: string) => {
-            accessToken.value = token;
-        };
-        const updateRefreshToken = (token: string) => {
-            refreshToken.value = token;
-        };
 
         return {
             refreshToken,
             accessToken,
-            updateAccessToken,
-            updateRefreshToken,
         };
     },
-    {
-        // default : localStorage
-        // refreshToken will be stored with the key name 'ATIBO'
-        persist: {
-            key: 'ATIBO',
-            paths: ['refreshToken'],
+    actions: {
+        updateAccessToken(token: string) {
+            this.accessToken = token;
         },
-    }
-);
+        updateRefreshToken(token: string) {
+            this.refreshToken = token;
+        },
+    },
+    // default : localStorage
+    // refreshToken will be stored with the key name 'ATIBO'
+    persist: {
+        key: 'ATIBO',
+        paths: ['refreshToken'],
+    },
+});
