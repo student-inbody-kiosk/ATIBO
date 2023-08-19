9b60e4ba57e5a2ed663f6c365c21d16db37125d3
kd <csb5268@gmail.com>
2023-08-20 01:55:02 +0900

    BE:init: create pjt

---
+++ backend_spring/Atibo/src/main/java/Atibo/Atibo/AtiboApplication.java
@@ -0,0 +1,13 @@
+package Atibo.Atibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiboApplication.class, args);
+	}
+
+}
---
+++ backend_spring/Atibo/src/test/java/Atibo/Atibo/AtiboApplicationTests.java
@@ -0,0 +1,13 @@
+package Atibo.Atibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
